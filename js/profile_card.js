9be1cbeda2e2eb9bff871f7183eef06212d37536
Nawod <madhuvanthanawo1@gmail.com>
2021-11-04 02:17:03 +0530

    add contact and profile pages

---
+++ js/profile_card.js
@@ -0,0 +1,49 @@
+const img = document.querySelector(".profile-img");
+const icons = document.querySelector(".icons");
+img.onclick = function(){
+  this.classList.toggle("active");
+  icons.classList.toggle("active");
+}
+
+
+function hideName1() {
+  document.querySelector("#outer-hide1").style.display = "none";
+}
+function showName1() {
+  document.querySelector("#outer-hide1").style.display = "block";
+}
+
+function hideName2() {
+  document.querySelector("#outer-hide2").style.display = "none";
+}
+function showName2() {
+  document.querySelector("#outer-hide2").style.display = "block";
+}
+
+function hideName3() {
+  document.querySelector("#outer-hide3").style.display = "none";
+}
+function showName3() {
+  document.querySelector("#outer-hide3").style.display = "block";
+}
+
+function hideName4() {
+  document.querySelector("#outer-hide4").style.display = "none";
+}
+function showName4() {
+  document.querySelector("#outer-hide4").style.display = "block";
+}
+
+function hideName5() {
+  document.querySelector("#outer-hide5").style.display = "none";
+}
+function showName5() {
+  document.querySelector("#outer-hide5").style.display = "block";
+}
+
+function hideName6() {
+  document.querySelector("#outer-hide6").style.display = "none";
+}
+function showName6() {
+  document.querySelector("#outer-hide6").style.display = "block";
+}
